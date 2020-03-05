3f225144a32e845ac50729690a884d1068153ed9
Smith Michael S <smithmichaels@johndeere.com>
2020-03-05 08:51:56 -0600

    Updates

---
+++ Python/frequent.py
@@ -0,0 +1,13 @@
+def frequent(arr):
+    newdict = {}
+    cntr = 0
+    element = ''
+    for indval in arr:
+        newdict[indval] = newdict.get(indval,0) + 1
+        if newdict[indval] >= cntr:
+            cntr = newdict[indval]
+            element = indval
+    print('Element is ', element)
+    print('Cntr is', cntr)
+
+frequent([2, 1, 2, 2, 1, 3])
